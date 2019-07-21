d75e4363e5913b922ca29ab797d7ac10758c87d9
orhun <drorhunvural@gmail.com>
2019-07-20 22:50:32 -0500

    Register, Login, Logout

--- application.py
+++ application.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from models import db
+from models import db # problem here
 from config import *
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 application = app = Flask(__name__) # application variable is needed when deploy to AWS
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
+bcrypt = Bcrypt(app) # to encrypt passwords
+
+login_manager = LoginManager(app) 
+login_manager.init_app(app)
 
 import views,errors # adding all the routing from the views.py
 
 
-#Example Orhun Vural
+
 
 if __name__ == '__main__':
     app.run()
